152b6e3ae1171a3175f49598c963b24623232e41
LSauter68140 <loic.sauter@gmail.com>
2022-12-22 16:04:13 +0100

    add popupMenu for setting alarm

--- app/src/main/java/fr/utt/if26/insanealarm/ui/alarm/AlarmFragment.java
+++ app/src/main/java/fr/utt/if26/insanealarm/ui/alarm/AlarmFragment.java
@@ -35,7 +35,6 @@ public class AlarmFragment extends Fragment {
 
         alarmViewModel.getAllAlarms().observe(getViewLifecycleOwner(), adapter::submitList);
         recyclerView.setAdapter(adapter);
-        //recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return root;
     }
--- app/src/main/java/fr/utt/if26/insanealarm/ui/alarm/AlarmViewHolder.java
+++ app/src/main/java/fr/utt/if26/insanealarm/ui/alarm/AlarmViewHolder.java
@@ -1,13 +1,21 @@
 package fr.utt.if26.insanealarm.ui.alarm;
 
+import static fr.utt.if26.insanealarm.R.id;
+import static fr.utt.if26.insanealarm.R.layout;
+import static fr.utt.if26.insanealarm.R.menu;
+import static fr.utt.if26.insanealarm.R.string;
+
 import android.annotation.SuppressLint;
 import android.content.res.Resources;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,25 +36,55 @@ public class AlarmViewHolder extends RecyclerView.ViewHolder {
     private final Switch activateAlarm;
     private final TextView type;
 
+    @SuppressLint("NonConstantResourceId")
     public AlarmViewHolder(@NonNull View itemView) {
         super(itemView);
-        nextRingTime = itemView.findViewById(R.id.listItemNextRingTime);
-        frequency = itemView.findViewById(R.id.listItemFrequency);
-        settingBtn = itemView.findViewById(R.id.listItemSettingBtn);
-        activateAlarm = itemView.findViewById(R.id.listItemActivateAlarm);
-        type = itemView.findViewById(R.id.listItemType);
+        nextRingTime = itemView.findViewById(id.listItemNextRingTime);
+        frequency = itemView.findViewById(id.listItemFrequency);
+        settingBtn = itemView.findViewById(id.listItemSettingBtn);
+        activateAlarm = itemView.findViewById(id.listItemActivateAlarm);
+        type = itemView.findViewById(id.listItemType);
+
+
     }
 
+
+    @SuppressLint("NonConstantResourceId")
     public void bind(Alarm alarm) {
         nextRingTime.setText(formatNextTimeToGoOff(alarm.getTimeToGoOff()));
         frequency.setText(formatFrequency(alarm, itemView.getResources()));
         activateAlarm.setChecked(alarm.getActivate());
         type.setText(String.valueOf(alarm.getAlarmType()));
+        settingBtn.setOnClickListener(view ->
+        {
+            PopupMenu popupMenu = new PopupMenu(settingBtn.getContext(), settingBtn, R.style.alarmSettingPopupMenu);
+            popupMenu.inflate(menu.alarm_setting_context);
+            popupMenu.setGravity(Gravity.END);
+            popupMenu.setOnMenuItemClickListener(item -> {
+                switch (item.getItemId()) {
+                    case id.alarmSettingEdit:
+                        Toast.makeText(settingBtn.getContext(), "Editer " + alarm.getName(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    case id.alarmSettingPreview:
+                        Toast.makeText(itemView.getContext(), "Visualiser", Toast.LENGTH_SHORT).show();
+
+                        return true;
+                    case id.alarmSettingDelete:
+                        Toast.makeText(itemView.getContext(), "supprimer", Toast.LENGTH_SHORT).show();
+
+                        return true;
+                    default:
+                        return false;
+                }
+            });
+            //displaying the popup
+            popupMenu.show();
+        });
     }
 
     static AlarmViewHolder create(ViewGroup parent) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.alarm_list_item, parent, false);
+                .inflate(layout.alarm_list_item, parent, false);
         return new AlarmViewHolder(view);
     }
 
@@ -57,7 +95,7 @@ public class AlarmViewHolder extends RecyclerView.ViewHolder {
     private String formatFrequency(Alarm alarm, Resources resources) {
         ArrayList<String> validDays = alarm.getFrequency().getWeekSchedule();
         if (validDays.size() == 0) {
-            return resources.getString(R.string.unique);
+            return resources.getString(string.unique);
         }
 
         Boolean isShort = validDays.size() < 3;
@@ -69,4 +107,6 @@ public class AlarmViewHolder extends RecyclerView.ViewHolder {
         }
         return weekSummary.substring(0, weekSummary.length() - 1);
     }
+
+
 }
